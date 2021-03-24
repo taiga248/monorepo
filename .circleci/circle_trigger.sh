86adf8e367699d31661edbff0e2f96d3c832a9b7
t.ito <t.ito@sonicmoov.com>
2021-03-24 19:54:18 +0900

    Update

--- .circleci/circle_trigger.sh
+++ .circleci/circle_trigger.sh
@@ -114,8 +114,8 @@ DATA="{ \"branch\": \"$CIRCLE_BRANCH\", \"parameters\": { $PARAMETERS } }"
 echo "Triggering pipeline with data:"
 echo -e "  $DATA"
 
-# URL="${CIRCLE_API}/v2/project/${REPOSITORY_TYPE}/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/pipeline"
-URL="${CIRCLE_API}/v2/project/github/taiga248/monorepo/pipeline"
+URL="${CIRCLE_API}/v2/project/${REPOSITORY_TYPE}/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/pipeline"
+# URL="${CIRCLE_API}/v2/project/github/taiga248/monorepo/pipeline"
 HTTP_RESPONSE=$(curl -s -u "3d15c748856dfe0a25ef41f40f3db05bddbce68a:" -o response.txt -w "%{http_code}" -X POST --header "Content-Type: application/json" -d "$DATA" "$URL")
 
 if [ "$HTTP_RESPONSE" -ge "200" ] && [ "$HTTP_RESPONSE" -lt "300" ]; then
