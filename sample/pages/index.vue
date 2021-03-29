fb3df77b46cfabf87f2a746bf4e946d86e9f32ee
t.ito <t.ito@sonicmoov.com>
2021-03-29 13:23:16 +0900

    Update

--- sample/pages/index.vue
+++ sample/pages/index.vue
@@ -2,24 +2,6 @@
   <div class="container">
     <div>
       <h1 class="title">sample</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
     </div>
   </div>
 </template>
