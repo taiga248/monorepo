1766c76636248a0c8f7044ae83059020be2417b9
t.ito <t.ito@sonicmoov.com>
2021-06-01 12:11:08 +0900

    demo update test, echo URL and HTTP_RESPONSE

--- .circleci/circle_trigger.sh
+++ .circleci/circle_trigger.sh
@@ -116,6 +116,10 @@ echo -e "  $DATA"
 
 URL="${CIRCLE_API}/v2/project/${REPOSITORY_TYPE}/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/pipeline"
 HTTP_RESPONSE=$(curl -s -u "${CIRCLE_TOKEN}:" -o response.txt -w "%{http_code}" -X POST --header "Content-Type: application/json" -d "$DATA" "$URL")
+echo "---------"
+echo URL
+echo HTTP_RESPONSE
+echo "---------"
 
 if [ "$HTTP_RESPONSE" -ge "200" ] && [ "$HTTP_RESPONSE" -lt "300" ]; then
     echo "API call succeeded."
