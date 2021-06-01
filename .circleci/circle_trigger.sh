093f881e6b7a2aa34f386690e7c03b01cee08763
t.ito <t.ito@sonicmoov.com>
2021-06-01 12:17:29 +0900

    Update

--- .circleci/circle_trigger.sh
+++ .circleci/circle_trigger.sh
@@ -115,7 +115,7 @@ echo "Triggering pipeline with data:"
 echo -e "  $DATA"
 
 $URL="${CIRCLE_API}/v2/project/${REPOSITORY_TYPE}/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/pipeline"
-$HTTP_RESPONSE=$(curl -s -u "${CIRCLE_TOKEN}:" -o response.txt -w "%{http_code}" -X POST --header "Content-Type: application/json" -d "$DATA" "$URL")
+$HTTP_RESPONSE="$(curl -s -u "${CIRCLE_TOKEN}:" -o response.txt -w "%{http_code}" -X POST --header "Content-Type: application/json" -d "$DATA" "$URL")"
 echo "---------"
 echo $URL
 echo $HTTP_RESPONSE
